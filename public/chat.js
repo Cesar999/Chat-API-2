de7905077f215afe405369ea31a95729fdaf3aca
Cesar Encinas <cesar_rer@hotmail.com>
2018-09-11 22:08:30 -0700

    Chat with Private Conversations

---
+++ public/chat.js
@@ -0,0 +1,173 @@
+const socket = io.connect();
+
+const users_memory = [];
+let mainUser='';
+let route_user;
+
+first_logIn();
+
+//functions----------------------------------
+
+function changeContent(node){
+    let mainContent = document.getElementById("main-content");
+
+    while (mainContent.firstChild) {
+        mainContent.removeChild(mainContent.firstChild);
+    }
+
+    const temp = document.getElementById(node);
+    const cloned = temp.content.cloneNode(true);
+    mainContent.appendChild(cloned);
+}
+
+function first_logIn(){
+    changeContent("temp-form-logIn");
+    const btnlogIn = document.getElementById('btn-logIn');
+    const signUp = document.getElementById('btn-to-signUp');
+
+    btnlogIn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        logIn_User();
+    });
+
+    signUp.addEventListener('click',(e)=>{
+        e.preventDefault();
+        changeContent("temp-form-signUp");
+
+        const btnSignUp = document.getElementById('btn-signUp');
+        btnSignUp.addEventListener('click',()=>{
+            signUp_User();
+        });
+
+    });
+}
+
+function logIn_User(){
+    const usernameLogIn = document.getElementById('username-logIn');
+    const passwordLogIn = document.getElementById('password-logIn');
+    socket.emit('login',{username: usernameLogIn.value, password: passwordLogIn.value},(data)=>{
+        alert(data.msg);
+        if(data.msg==='Access Allowed'){
+            startChat(data.user);
+        }
+    });
+}
+
+function signUp_User(){
+    const usernameSignUp = document.getElementById('username-signUp');
+    const password1SignUp = document.getElementById('password1-signUp');
+    const password2SignUp = document.getElementById('password2-signUp');
+
+    if(usernameSignUp.value.includes(" ")||password1SignUp.value.includes(" ")||usernameSignUp.value.length<3||password1SignUp.value.length<3){
+        alert('username/password greater than 2. Do not use white space');
+    }else{
+        if(password1SignUp.value===password2SignUp.value){
+            socket.emit('signin',{username: usernameSignUp.value, password: password1SignUp.value},(data)=>{
+                if(data.notRepeatUser){
+                    alert('Succesfully Sign In');
+                    startChat(data.user);
+                }else{
+                    alert('Username already taken');
+                }
+            });
+        }
+        else{
+            alert('Passwords are not the same');
+        }
+    }
+}
+
+function startChat(username){
+    mainUser = username;
+    changeContent("temp-chat-content");
+    document.getElementById('title-username').innerHTML=username;
+
+    const btnLogOut = document.getElementById('btn-logOut');
+    btnLogOut.addEventListener('click',(e)=>{
+        e.preventDefault();
+        first_logIn();
+    });
+
+    socket.emit('user online',{username});
+
+    let sendBtn = document.getElementById('btn-send');
+    let sendBox = document.getElementById('box-send');
+
+    sendBox.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            sendBtn.click();
+        }
+    });
+
+    sendBtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        socket.emit('send message',{msg: sendBox.value, to: route_user});
+        sendBox.value = '';
+    });
+
+}
+
+
+socket.on('list usernames',(data)=>{
+    let arr = data.users;
+    console.log(arr);
+    let listUsers = document.getElementById('list-users');
+
+    li_arr = listUsers.getElementsByTagName('LI');
+    li_arr = Array.from(li_arr);
+    for(li of li_arr){
+        if(li.firstChild.innerHTML!==`Global`){
+            li.remove();
+        }
+    }
+
+
+    for(user of arr){
+        let li = document.createElement('li');
+        li.innerHTML = `<a href='#'>${user}</a>`;
+        li.className = 'li-users';
+        li.firstChild.addEventListener('click',function(e){
+            route_user = e.target.innerText;
+            let chatTitle = document.getElementById('title-username')
+            let username = mainUser;
+            chatTitle.innerText = '';
+            chatTitle.innerText = `${username} to ${route_user}`;
+            changeChat();
+        });
+        listUsers.appendChild(li);
+
+        if(users_memory.includes(user)){}
+        else{
+            users_memory.push(user);
+            let wrapper = document.getElementById(`chat-area-wrapper`);
+            let node = document.createElement('div');
+            node.id = `chat-area-${user}`;
+            node.className = `chat-area-class`;
+            node.style.display = 'none';
+            wrapper.appendChild(node);
+        }
+    }
+    
+});
+
+function changeChat(){
+    let wrapper = document.getElementById(`chat-area-wrapper`);
+    let d_array = wrapper.getElementsByTagName('DIV');
+    d_array = Array.from(d_array);
+
+        for(element of d_array){
+            if(element.id===`chat-area-${route_user}`){
+                element.style.display = 'block';
+                temp = `chat-area-${route_user}`;
+            }
+            else{
+                element.style.display = 'none'; 
+            }
+        }
+}
+
+socket.on('private',(data)=>{
+    let chatBox = document.getElementById(`chat-area-${data.to}`);
+    chatBox.innerHTML += `<span class='whisper'><strong>${data.nick}</strong>: ${data.msg}</span<><br/>`;
+});
---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require(`express`);
+const socket = require('socket.io');
+const mongoose = require('mongoose');
+
+//START MONGOOSE---------------------------------------
+mongoose.connect('mongodb://localhost/chatAPI2');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {type: String},
+    password: {type: String}
+});
+const User = mongoose.model('user',userSchema);
+//END MONGOOSE---------------------------------------
+
+//START EXPRESS-----------------------------------
+const app = express();
+const port = 3000;
+
+var server = app.listen(port,()=>{
+    console.log(`Listening port 3000`);
+});
+
+let publicPath = __dirname+'/public';
+app.use(express.static(publicPath));
+
+//END EXPRESS-----------------------------------
+
+const users = {};
+
+//START SOCKETS-----------------------------------
+const io = socket(server);
+io.sockets.on('connection',(socket)=>{
+    
+    socket.on('signin',(data, callback)=>{
+        User.find({username: data.username})
+            .then((users)=>{
+                if(users.length<1){
+                    const user1 = new User({username: data.username, password: data.password});
+                    user1.save();
+                    callback({notRepeatUser:true, user:user1.username});
+                }else{
+                    callback({notRepeatUser:false, user:''});
+                }
+            });
+    });  
+
+    socket.on('login',(user, callback)=>{
+        User.find({username: user.username})
+            .then((users)=>{
+                let flag1 = user.username===users[0].username;
+                let flag2 = user.password===users[0].password;
+                if(flag1&&flag2){
+                    callback({msg:'Access Allowed', user:user.username});
+                }
+                else{
+                    callback({msg:'Wrong Password', user:''});
+                }
+        }).catch((e)=>{
+            console.log(e);
+            callback({msg:'Username does not exist', user:''});
+        });
+    });
+
+    socket.on('user online',(user)=>{
+        socket.nickname = user.username;
+        users[socket.nickname]=socket;
+        io.sockets.emit('list usernames',{users:Object.keys(users)});
+    });
+
+    socket.on('send message',(data)=>{
+        users[data.to].emit('private', {msg: data.msg, nick: data.to, to: socket.nickname});
+
+        users[socket.nickname].emit('private', {msg: data.msg, nick: socket.nickname, to: data.to});
+    });
+
+    socket.on('disconnect',()=>{ 
+        if(!socket.nickname){
+            return;
+        }
+        delete users[socket.nickname];
+        io.sockets.emit('list usernames', {users:Object.keys(users)});
+    });
+
+}); 
+//END SOCKETS-----------------------------------
